9892ee53c7c283e1eef8f92212795d16d38e9764
Eric De Witte <vEDW@users.noreply.github.com>
2023-09-26 16:09:54 +0200

    nullify
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- pvc-app/set_zone.sh
+++ pvc-app/set_zone.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 DEMOAPP=$(yq e statefulset-topology-aware.yaml)
-DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = []' -)
+DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = null' -)
 ZONES=$(kubectl get nodes -o json |jq -r '.items[].metadata.labels."failure-domain.beta.kubernetes.io/zone"' | sort | uniq)
 for ZONE in $ZONES; do
     DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values += ["'"$ZONE"'"]' -)
 done
 echo "${DEMOAPP}" > demo-app.yaml
+yq e demo-app.yaml
