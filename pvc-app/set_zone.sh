c6d02d5e67a39a68c678d8ac0ef320bc54aaf2b7
Eric De Witte <vEDW@users.noreply.github.com>
2023-09-26 16:07:46 +0200

    empty array
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- pvc-app/set_zone.sh
+++ pvc-app/set_zone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DEMOAPP=$(yq e statefulset-topology-aware.yaml)
-DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = [""]' -)
+DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = []' -)
 ZONES=$(kubectl get nodes -o json |jq -r '.items[].metadata.labels."failure-domain.beta.kubernetes.io/zone"' | sort | uniq)
 for ZONE in $ZONES; do
     DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values += ["'"$ZONE"'"]' -)
