5ead0ef0d6c5cb975a3edf63ee30df95f424fe23
Eric De Witte <vEDW@users.noreply.github.com>
2023-09-27 18:31:21 +0200

    set zone demo app
    Signed-off-by: Eric De Witte <vEDW@users.noreply.github.com>

--- pvc-app/set_zone.sh
+++ pvc-app/set_zone.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 DEMOAPP=$(yq e statefulset-topology-aware.yaml)
-DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = null' -)
+DEMOAPP=$(echo "${DEMOAPP}" | yq e 'select(.kind=="Service"),select(.kind=="StatefulSet")|.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values = null' -)
 ZONES=$(kubectl get nodes -o json |jq -r '.items[].metadata.labels."failure-domain.beta.kubernetes.io/zone"' | sort | uniq)
 for ZONE in $ZONES; do
-    DEMOAPP=$(echo "${DEMOAPP}" | yq e '.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values += ["'"$ZONE"'"]' -)
+    DEMOAPP=$(echo "${DEMOAPP}" | yq e 'select(.kind=="Service"),select(.kind=="StatefulSet")|.spec.template.spec.affinity.nodeAffinity.requiredDuringSchedulingIgnoredDuringExecution.nodeSelectorTerms[].matchExpressions[].values += ["'"$ZONE"'"]' -)
 done
 echo "${DEMOAPP}" > demo-app.yaml
 yq e demo-app.yaml
